3ece43437eb497fd5c8bd1920a1cbca33b91847b
Peter Cosemans <peter.cosemans@gmail.com>
2018-06-13 12:17:53 +0200

    Add webpack bundle analyses

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,24 @@
+/* eslint-disable */
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+
 module.exports = {
   lintOnSave: false,
+  pluginOptions: {
+    enalbeInSFC: false, // Locale messages in Single file components
+  },
+  configureWebpack: {
+    performance: {
+      hints: process.env.NODE_ENV === 'production' ? 'warning' : false,
+    },
+  },
+  chainWebpack: config => {
+    if (process.env.ANALYZE === 'true')
+      config.plugin('webpack-report').use(BundleAnalyzerPlugin, [
+        {
+          analyzerMode: 'static',
+        },
+      ]);
+  },
 };
